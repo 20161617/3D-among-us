505cd8fe5459565eee7e144dd4ec0550af32a62f
Johs123 <zlwlwhs25@game.hoseo.edu>
2020-12-04 04:18:26 +0900

    RoomPrefab

---
+++ amongUs/Assets/02.Scripts/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using static NetworkManager;
+
+public class UIManager : MonoBehaviourPun
+{
+
+    public static UIManager UM;
+    public Button StartBtn;
+
+    void Awake()
+    {
+        UM = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!PhotonNetwork.IsMasterClient) return;
+        ShowStartBtn();
+    }
+    void ShowStartBtn()
+    {
+        StartBtn.gameObject.SetActive(true);
+        StartBtn.interactable = PhotonNetwork.CurrentRoom.PlayerCount >= 2;
+    }
+}
