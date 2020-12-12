084fee93691bfb1292fbb7ceb3399aa6502fc4b4
LeeNamGwan <70495598+LeeNamGwan@users.noreply.github.com>
2020-12-13 00:53:27 +0900

    쓰레기 버리기 미션추가
    쓰레기 버리기 미션추가

---
+++ amongUs/Assets/02.Scripts/MissonScripts/UploadGague.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UploadGague : MonoBehaviour
+{
+    public Image fillGague;
+    public Text gagueText;
+
+
+    public void gagueSet(bool set)
+    {
+        gameObject.SetActive(set);
+    }
+    public void FillGague()
+    {
+        fillGague.fillAmount += 0.1f;
+        gagueText.text = (int)(fillGague.fillAmount * 100)+"%";
+    }
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
