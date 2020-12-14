dd32a871f73f99fd9522111c2536a005f79ece78
LeeNamGwan <70495598+LeeNamGwan@users.noreply.github.com>
2020-12-15 00:57:46 +0900

    병합
    병합

--- amongUs/Assets/02.Scripts/PlayerScript/TargetCtrl.cs
+++ amongUs/Assets/02.Scripts/PlayerScript/TargetCtrl.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using static MissionManager;
+using static DatabaseManager;
 
 public class TargetCtrl : MonoBehaviourPun
 {
@@ -58,10 +59,15 @@ public class TargetCtrl : MonoBehaviourPun
                 TargetMissionData = selection.GetComponent<MissionData>();
 
                 //선택한 오브젝트의 미션데이터가 현재 임무에 포함되어 있다면
-                if (MissionManager.Instance.ContainsMission(TargetMissionData.MissionType, TargetMissionData.MissionNumber))
+                if (MissionManager.Instance.ContainsMission(TargetMissionData.MissionType, TargetMissionData.MissionNumber, databaseManager.MyPlayer.isImposter))
                 {
-                    //반짝이를 켜준다
-                    GlowObject selectionGlowObject = selection.GetComponent<GlowObject>();
+                    if (TargetMissionData.MissionType == MissionCommon || TargetMissionData.MissionType == MissionSimple || TargetMissionData.MissionType == MissionDifficult) //미션 타입 
+                    {
+                        if (!transform.GetComponent<PlayerMission>().myMission.Contains(selection.gameObject))
+                            return;
+                    }
+                        //반짝이를 켜준다
+                        GlowObject selectionGlowObject = selection.GetComponent<GlowObject>();
 
                     selectionGlowObject.OnRaycastEnter();
 
@@ -76,10 +82,16 @@ public class TargetCtrl : MonoBehaviourPun
             Debug.Log(hit.collider.name);
         }
     }
-
     //Use버튼을 누르면 현재 미션데이터에 맞는 미션을 불러오는 함수를 호출
     public void TargetUse()
     {
         MissionManager.Instance.CallMission(TargetMissionData.MissionType, TargetMissionData.MissionNumber);
+    }
+    //Use버튼을 누르면 현재 미션데이터에 맞는 미션을 불러오는 함수를 호출
+    public void TargetKill()
+    {
+        Debug.Log("죽여!");
+        databaseManager.MyPlayer.GetComponent<PhotonView>().RPC("KillRPC", RpcTarget.AllViaServer);
+         //_selection.GetComponent<PlayerScript>().KillingPlayer();
     }
 }
